65241593907a37c993f767421b83bf71acb0b350
sarir-karim <sarirkarim755@gmail.com>
2024-09-10 00:48:09 +0500

    start chapter6

---
+++ chapter6.cpp
@@ -0,0 +1,10 @@
+// Binary number system;
+
+//In matimatics we have learned about the Decimal number system whcih starts from 0 and ends at 9; aslo called bas10 number system becuase it has 10 digits;
+/*
+Now the computer only understads the binary number system which is 0 and 1;
+binary is called base2 number system
+octal is called base8 number system
+hexadecimal is called base16 number system
+
+*/
